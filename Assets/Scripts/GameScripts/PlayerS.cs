b3e2021781f3a5d508ed7a375025cb06f73d74b7
tonybrand <tonybrandTB37@gmail.com>
2023-10-13 16:09:31 +0800

    修改主角的移动

--- Assets/Scripts/GameScripts/PlayerS.cs
+++ Assets/Scripts/GameScripts/PlayerS.cs
@@ -5,38 +5,92 @@ using UnityEngine;
 
 public class PlayerS : MonoBehaviour
 {
+    float moveHorizontal;
+    float moveVertical;
+    [SerializeField]
+    Vector3 movement;
+    Rigidbody2D rb2d;
+
     public float speed = 5f;
     public GameObject catFood;
 
+    public float moveSpeed = 5.0f;  // 正常移动速度
+    public float sprintSpeed = 10.0f;  // 加速移动速度
+
+    [SerializeField]
+    private float currentSpeed;  // 当前速度
+
     public static event Action<GameObject> OnCatFoodGenerated;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 开始时设定当前速度为正常移动速度
+        currentSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(horizontalInput, verticalInput) * speed * Time.deltaTime;
-        transform.Translate(movement);
+        GetInput(); 
+        GenerateCatFood();        
+    }
 
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
-        // 如果按下空格键，则生成物体B
-        if (Input.GetKeyDown(KeyCode.Space))
+    void GenerateCatFood()
+    {
+        // 生成物体B
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            GenerateCatFood(transform.position + transform.forward * 2f);  // 在物体C的前方稍远的位置生成物体B,这里可以改成玩家面朝的方向
+            Vector3 pos = transform.position + transform.forward * 2f;
+            GameObject objectB = Instantiate(catFood, pos, Quaternion.identity);
+
+            // 触发事件，通知其他订阅了这个事件的脚本
+            OnCatFoodGenerated?.Invoke(objectB);
+        }        
+    }
+
+    void GetInput() 
+    {
+        // 获取方向键的输入
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        // 创建移动向量
+        movement = new Vector3(moveHorizontal, moveVertical, 0.0f).normalized;
+
+        // 判断是否按下加速键 (例如，Shift键)
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = moveSpeed;
         }
     }
 
-    void GenerateCatFood(Vector3 position)
+    void MovePlayer()
     {
-        // 生成物体B
-        GameObject objectB = Instantiate(catFood, position, Quaternion.identity);
-        // 触发事件，通知其他订阅了这个事件的脚本
-        OnCatFoodGenerated?.Invoke(objectB);
+        // 如果使用Rigidbody2D移动，优先使用velocity属性进行移动
+        
+        if (rb2d != null)
+        {
+            rb2d.velocity = movement * currentSpeed;
+        }
+        else
+        {
+            // 否则还是使用Transform来移动
+            transform.position += movement * currentSpeed * Time.deltaTime;
+        }
     }
 }
